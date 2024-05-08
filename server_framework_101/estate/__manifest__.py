a65441bd9bfb3afc9e22a09d086819adfaf956a6
Luis Cabañero <Luis.Cabanero@uclm.es>
2024-05-08 11:43:01 +0200

    Fixed bug related to load order of views that made imposible to install the module.

--- server_framework_101/estate/__manifest__.py
+++ server_framework_101/estate/__manifest__.py
@@ -4,10 +4,10 @@
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_offer_views.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
-        "views/estate_property_offer_views.xml",
         "views/user_views.xml",
         "views/estate_menus.xml",
     ],
